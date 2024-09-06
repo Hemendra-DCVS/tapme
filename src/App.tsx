a849a0aeb6befcaa7be5aafa2aae4692c03955ba
Hemendra-DCVS <hemendrachanti@gmail.com>
2024-09-06 10:11:20 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 // In src/App.tsx
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { ApolloClient, InMemoryCache, gql, useMutation } from '@apollo/client';
 
 const client = new ApolloClient({
